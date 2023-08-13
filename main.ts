34e45613854ac3b59c40f0556de78bfe30c762e2
larryfast <larry@legible.com>
2023-08-13 11:28:38 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ radio.setGroup(12)
 basic.forever(function () {
     temperature = input.temperature()
     radio.sendValue(control.deviceName(), temperature)
+    serial.writeValue(control.deviceName(), temperature)
     basic.showString("" + (temperature))
     basic.pause(1000)
 })
